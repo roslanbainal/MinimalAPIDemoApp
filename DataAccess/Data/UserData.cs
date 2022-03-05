6b26d33d9deb75856968c5726126099aafaea755
roslan bainal <roslan@simpleadvantage.com>
2022-03-05 19:58:45 +0800

    Add project files.

---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess.DBAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class UserData : IUserData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public UserData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<IEnumerable<UserModel>> GetUsers() =>
+            _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+
+        public async Task<UserModel?> GetUser(int id)
+        {
+            var result = await _db.LoadData<UserModel, dynamic>(
+                "dbo.spUser_Get",
+                new { Id = id });
+
+            return result.FirstOrDefault();
+        }
+
+        public Task InsertUser(UserModel user) =>
+            _db.SaveDate("dbo.spUser_Insert",
+                new { user.FirstName, user.LastName });
+
+        public Task UpdateUser(UserModel user) =>
+            _db.SaveDate("dbo.spUser_Update", user);
+
+        public Task DeleteUser(int id) =>
+            _db.SaveDate("dbo.spUser_Delete", new { Id = id });
+
+    }
+}
---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	values ('Roslan','Bainal'),
+	('Anis', 'Kamal'),
+	('Siti', 'Abu'),
+	('Ali', 'Bakar');
+end
